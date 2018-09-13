5efad4657d6cbc21cbded43f8d9359cb467a5981
Corentin Schreiber <cschreib@orange.fr>
2018-09-13 14:04:51 +0100

    Pushed to v2.0

--- src/egg-gencat.cpp
+++ src/egg-gencat.cpp
@@ -1828,7 +1828,7 @@ void print_help() {
         }
     };
 
-    print("egg-gencat v1.1.0");
+    print("egg-gencat v1.2.0");
     print("usage: egg-gencat [options]\n");
 
     print("List of generic options:");
